f16660dc1c3411dd87853cbc83dbcc7f9180bd99
Cody Buffaloe <codylbuffaloe@gmail.com>
2015-11-30 18:19:08 -0500

    Finished P4, Herons Formula

--- JavaScript_Exercises/W3Schools_JSBasics_P4.js
+++ JavaScript_Exercises/W3Schools_JSBasics_P4.js
@@ -1,1 +1,8 @@
-W3Schools_JSBasics_P4.js
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+
+var semiPerimeter = (side1 + side2+ side3)/2;
+var area = Math.sqrt(semiPerimeter(semiPerimeter - side1)(semiPerimeter-side2)(semiPerimeter-side3)));
+console.log(area);
+//Heron's Solution is what this is known as.
